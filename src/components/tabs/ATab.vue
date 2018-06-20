477171da721b888b5dfd0eb9c7635765a3492d83
mateusz.littwin <mateusz.littwin@gforces.pl>
2018-06-20 23:13:30 +0200

    Add there ways to two-way-binding between Root and Child components

---
+++ src/components/tabs/ATab.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="tab">
+        <h2>Hey I am A-Tab</h2>
+        <h4>It's event emmit way to change value.</h4>
+        {{ content }}
+        <div>
+            <input type="text" :value="content" @input="onChange($event.target)">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        content: {
+            type: String,
+        }
+    },
+
+    methods: {
+        onChange (target) {
+            this.$emit('input', target.value);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './../../assets/mixins';
+.tab {
+    @include tabStyle;
+}
+</style>
---
+++ src/components/tabsContainer.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="tabs-container">
+    <div>
+      <p>This is tabsContainer.vue component!</p>
+      {{ content }}
+      <button type="button" @click="restore">Restore</button>
+    </div>
+
+    <ATab :content="content" @input="onATabInput" />
+    <BTab v-model="content" />
+    <CTab :content="content" :callback="changeContent" />
+  </div>
+</template>
+
+<script>
+import ATab from './tabs/ATab.vue';
+import BTab from './tabs/BTab.vue';
+import CTab from './tabs/CTab.vue';
+
+const defaultContent = 'It comes from tabs container [tabsContainer.vue] component!';
+
+export default {
+  components: {
+    ATab, BTab, CTab,
+  },
+
+  data () {
+    return {
+      content: 'It comes from tabs container [tabsContainer.vue] component!',
+    }
+  },
+
+  methods: {
+    onATabInput (value) {
+      this.content = value;
+    },
+
+    restore () {
+      this.content = defaultContent;
+    },
+
+    changeContent (newContent) {
+      this.content = newContent;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import './../assets/mixins';
+.tabs-container {
+    @include tabStyle;
+    padding: 20px;
+}
+</style>
+
